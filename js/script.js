8f66ab6b7c5cd397629215bd1f2d9d59eb641c0d
Den <chester3359@gmail.com>
2023-01-08 12:07:36 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var name ='ivan';
+var store = 'sklep';
+
+const storeDescription = {
+    budget:10000,
+    employees : ['adam' , 'marek' , 'petr'],
+    products :  {pomidor:10,ogorek:5} ,
+    open : true
+};
